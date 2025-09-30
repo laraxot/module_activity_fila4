ecc38bd411831ea98326ca9b7338bf7495b3d496
marco76tv <marco.sottana@gmail.com>
2025-09-30 21:14:49 +0200

    .

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -9,7 +9,11 @@ use Modules\Xot\Filament\Pages\XotBaseDashboard;
 
 class Dashboard extends XotBaseDashboard
 {
+<<<<<<< HEAD
     //protected static ?string $navigationIcon = 'heroicon-o-home';
+=======
+    //protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-home';
+>>>>>>> e02cccb (.)
     //protected static string $view = 'activity::filament.pages.dashboard';
     // public function mount(): void
     // {
