4619d921c3885b6e060a40702bc007d119c898d1
marco76tv <marco.sottana@gmail.com>
2025-10-22 09:06:13 +0200

    .

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -9,8 +9,29 @@ use Modules\Xot\Filament\Pages\XotBaseDashboard;
 
 class Dashboard extends XotBaseDashboard
 {
+<<<<<<< HEAD
     //protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-home';
     //protected static string $view = 'activity::filament.pages.dashboard';
+=======
+    //protected static ?string $navigationIcon = 'heroicon-o-home';
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    //protected static string $view = 'activity::filament.pages.dashboard';
+=======
+
+    //protected static string $view = 'activity::filament.pages.dashboard';
+
+>>>>>>> a12f125f4a (.)
+=======
+    //protected static string $view = 'activity::filament.pages.dashboard';
+>>>>>>> b93ef594b4 (.)
+=======
+
+    //protected static string $view = 'activity::filament.pages.dashboard';
+
+>>>>>>> origin/develop
+>>>>>>> b1cd7fc (.)
     // public function mount(): void
     // {
     //     // $user = auth()->user();
