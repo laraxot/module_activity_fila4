0f4515a2027cda9f5e287b25214a795288207d5c
marco76tv <marco.sottana@gmail.com>
2025-11-11 10:38:47 +0100

    .

--- app/Models/Policies/SnapshotPolicy.php
+++ app/Models/Policies/SnapshotPolicy.php
@@ -6,15 +6,21 @@ namespace Modules\Activity\Models\Policies;
 
 use Modules\Activity\Models\Snapshot;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 18dcd64 (.)
 use Modules\User\Models\Policies\UserBasePolicy;
 use Modules\Xot\Contracts\UserContract;
 
 class SnapshotPolicy extends UserBasePolicy
+<<<<<<< HEAD
 =======
 use Modules\Xot\Contracts\UserContract;
 
 class SnapshotPolicy extends ActivityBasePolicy
 >>>>>>> 0a00ff2 (.)
+=======
+>>>>>>> 18dcd64 (.)
 {
     /**
      * Determine whether the user can view any models.
@@ -28,10 +34,14 @@ class SnapshotPolicy extends ActivityBasePolicy
      * Determine whether the user can view the model.
      */
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function view(UserContract $user, Snapshot $_snapshot): bool
 =======
     public function view(UserContract $user, Snapshot $snapshot): bool
 >>>>>>> 0a00ff2 (.)
+=======
+    public function view(UserContract $user, Snapshot $_snapshot): bool
+>>>>>>> 18dcd64 (.)
     {
         return $user->hasPermissionTo('snapshot.view');
     }
@@ -48,10 +58,14 @@ class SnapshotPolicy extends ActivityBasePolicy
      * Determine whether the user can update the model.
      */
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function update(UserContract $user, Snapshot $_snapshot): bool
 =======
     public function update(UserContract $user, Snapshot $snapshot): bool
 >>>>>>> 0a00ff2 (.)
+=======
+    public function update(UserContract $user, Snapshot $_snapshot): bool
+>>>>>>> 18dcd64 (.)
     {
         return $user->hasPermissionTo('snapshot.update');
     }
@@ -60,10 +74,14 @@ class SnapshotPolicy extends ActivityBasePolicy
      * Determine whether the user can delete the model.
      */
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function delete(UserContract $user, Snapshot $_snapshot): bool
 =======
     public function delete(UserContract $user, Snapshot $snapshot): bool
 >>>>>>> 0a00ff2 (.)
+=======
+    public function delete(UserContract $user, Snapshot $_snapshot): bool
+>>>>>>> 18dcd64 (.)
     {
         return $user->hasPermissionTo('snapshot.delete');
     }
@@ -72,10 +90,14 @@ class SnapshotPolicy extends ActivityBasePolicy
      * Determine whether the user can restore the model.
      */
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function restore(UserContract $user, Snapshot $_snapshot): bool
 =======
     public function restore(UserContract $user, Snapshot $snapshot): bool
 >>>>>>> 0a00ff2 (.)
+=======
+    public function restore(UserContract $user, Snapshot $_snapshot): bool
+>>>>>>> 18dcd64 (.)
     {
         return $user->hasPermissionTo('snapshot.restore');
     }
@@ -88,7 +110,11 @@ class SnapshotPolicy extends ActivityBasePolicy
         return $user->hasPermissionTo('snapshot.forceDelete');
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
 }
 =======
 }
 >>>>>>> 0a00ff2 (.)
+=======
+}
+>>>>>>> 18dcd64 (.)
