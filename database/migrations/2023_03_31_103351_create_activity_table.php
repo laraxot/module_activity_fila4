708c367bde797d1579ebd665bed8518259ec90da
marco76tv <marco.sottana@gmail.com>
2025-11-11 10:39:31 +0100

    .

--- database/migrations/2023_03_31_103351_create_activity_table.php
+++ database/migrations/2023_03_31_103351_create_activity_table.php
@@ -10,7 +10,11 @@ return new class extends XotBaseMigration
     public function up(): void
     {
         $this->tableCreate(
+<<<<<<< HEAD
             function (Blueprint $table): void {
+=======
+            function (Blueprint $table) {
+>>>>>>> 9baa519 (.)
                 $table->bigIncrements('id');
                 $table->string('log_name')->nullable();
                 $table->text('description');
@@ -24,7 +28,11 @@ return new class extends XotBaseMigration
         );
 
         $this->tableUpdate(
+<<<<<<< HEAD
             function (Blueprint $table): void {
+=======
+            function (Blueprint $table) {
+>>>>>>> 9baa519 (.)
                 // Assicuriamoci che causer_id sia nullable se esiste
                 if ($this->hasColumn('causer_id')) {
                     $table->string('causer_id')->nullable()->change();
