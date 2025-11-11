b07c85fe24bc60e0ce0d1c5b22b93051938408cd
marco76tv <marco.sottana@gmail.com>
2025-11-11 10:23:29 +0100

    .

--- tests/Unit/Models/StoredEventBusinessLogicTest.php
+++ tests/Unit/Models/StoredEventBusinessLogicTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 use function Safe\class_uses;
 
+use function Safe\class_uses;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Activity\Models\StoredEvent;
 use Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent;
