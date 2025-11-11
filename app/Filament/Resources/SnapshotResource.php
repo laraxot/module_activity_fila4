20c41134f0f6efd5ad826c4432177185800ceb3f
marco76tv <marco.sottana@gmail.com>
2025-11-11 10:39:27 +0100

    .

--- app/Filament/Resources/SnapshotResource.php
+++ app/Filament/Resources/SnapshotResource.php
@@ -20,13 +20,13 @@ class SnapshotResource extends XotBaseResource
     #[Override]
     public static function getFormSchema(): array
     {
-        return array_values([
-            'model_type' => TextInput::make('model_type')->required()->maxLength(255),
-            'model_id' => TextInput::make('model_id')->numeric()->required(),
-            'state' => KeyValue::make('state')->columnSpanFull(),
-            'created_by_type' => TextInput::make('created_by_type')->maxLength(255),
-            'created_by_id' => TextInput::make('created_by_id')->numeric(),
-        ]);
+        return [
+            TextInput::make('model_type')->required()->maxLength(255),
+            TextInput::make('model_id')->numeric()->required(),
+            KeyValue::make('state')->columnSpanFull(),
+            TextInput::make('created_by_type')->maxLength(255),
+            TextInput::make('created_by_id')->numeric(),
+        ];
     }
 
     #[Override]
