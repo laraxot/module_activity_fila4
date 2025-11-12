adc4178150ba9f8073a4a46b34c3e93d44ea2502
marco76tv <marco.sottana@gmail.com>
2025-11-12 14:43:49 +0100

    .

--- tests/Unit/Models/ActivityTest.php
+++ tests/Unit/Models/ActivityTest.php
@@ -12,10 +12,32 @@ test('activity can be created', function () {
 
     expect($activity)
         ->toBeActivity()
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> b1cd7fc (.)
         ->and($activity->name)
         ->toBe('Test Activity')
         ->and($activity->description)
         ->toBe('Test Description');
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+        ->and($activity->name)->toBe('Test Activity')
+        ->and($activity->description)->toBe('Test Description');
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+        ->and($activity->name)->toBe('Test Activity')
+        ->and($activity->description)->toBe('Test Description');
+>>>>>>> origin/develop
+>>>>>>> b1cd7fc (.)
 });
 
 test('activity has required attributes', function () {
@@ -30,14 +52,62 @@ test('activity has required attributes', function () {
 
 test('activity can be soft deleted', function () {
     $activity = createActivity();
+<<<<<<< HEAD
 
     $activity->delete();
 
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+    $activity->delete();
+
+=======
+    
+    $activity->delete();
+    
+>>>>>>> a12f125f4a (.)
+=======
+
+    $activity->delete();
+
+>>>>>>> b93ef594b4 (.)
+=======
+    
+    $activity->delete();
+    
+>>>>>>> origin/develop
+>>>>>>> b1cd7fc (.)
     expect($activity->trashed())->toBeTrue();
 });
 
 test('activity factory creates valid instances', function () {
     $activity = Activity::factory()->make();
+<<<<<<< HEAD
 
     expect($activity)->toBeActivity()->and($activity->name)->toBeString()->and($activity->description)->toBeString();
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+    expect($activity)->toBeActivity()->and($activity->name)->toBeString()->and($activity->description)->toBeString();
+=======
+=======
+>>>>>>> origin/develop
+    
+    expect($activity)
+        ->toBeActivity()
+        ->and($activity->name)->toBeString()
+        ->and($activity->description)->toBeString();
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+
+    expect($activity)->toBeActivity()->and($activity->name)->toBeString()->and($activity->description)->toBeString();
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> b1cd7fc (.)
 });
