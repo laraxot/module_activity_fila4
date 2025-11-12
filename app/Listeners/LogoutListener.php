98164c0d76fbc7f3179ba3b30e16560eea473173
marco76tv <marco.sottana@gmail.com>
2025-11-12 09:45:30 +0100

    .

--- app/Listeners/LogoutListener.php
+++ app/Listeners/LogoutListener.php
@@ -11,15 +11,7 @@ class LogoutListener
     /**
      * Handle the event.
      */
-<<<<<<< HEAD
-<<<<<<< HEAD
     public function handle(Logout $_event): void
-=======
-    public function handle(Logout $event): void
->>>>>>> 0a00ff2 (.)
-=======
-    public function handle(Logout $_event): void
->>>>>>> 18dcd64 (.)
     {
         // Implementazione del logout tracking
     }
