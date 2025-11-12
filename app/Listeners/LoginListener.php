5298696927b0b75371ddccae927a0ddc437e0984
marco76tv <marco.sottana@gmail.com>
2025-11-12 09:47:20 +0100

    .

--- app/Listeners/LoginListener.php
+++ app/Listeners/LoginListener.php
@@ -11,7 +11,23 @@ class LoginListener
     /**
      * Handle the event.
      */
+<<<<<<< HEAD
     public function handle(Login $_event): void
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function handle(Login $_event): void
+=======
+    public function handle(Login $event): void
+>>>>>>> a12f125f4a (.)
+=======
+    public function handle(Login $_event): void
+>>>>>>> b93ef594b4 (.)
+=======
+    public function handle(Login $event): void
+>>>>>>> origin/develop
+>>>>>>> b1cd7fc (.)
     {
         // ...
     }
