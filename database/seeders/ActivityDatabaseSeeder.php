677e90ea45b13bc3e010ddbf6c5e4d45a4d48fe7
marco76tv <marco.sottana@gmail.com>
2025-11-17 10:56:17 +0100

    .

--- database/seeders/ActivityDatabaseSeeder.php
+++ database/seeders/ActivityDatabaseSeeder.php
@@ -4,21 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Activity\Database\Seeders;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-use Database\Seeders\ActivitySeeder;
-use Database\Seeders\SnapshotSeeder;
-use Database\Seeders\StoredEventSeeder;
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> b1cd7fc (.)
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
@@ -31,30 +16,6 @@ class ActivityDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-<<<<<<< HEAD
         $this->call([]);
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        $this->call([]);
-=======
-        $this->call([
-            ActivitySeeder::class,
-            SnapshotSeeder::class,
-            StoredEventSeeder::class,
-        ]);
->>>>>>> a12f125f4a (.)
-=======
-        $this->call([]);
->>>>>>> b93ef594b4 (.)
-=======
-        $this->call([
-            \Database\Seeders\ActivitySeeder::class,
-            \Database\Seeders\SnapshotSeeder::class,
-            \Database\Seeders\StoredEventSeeder::class,
-        ]);
->>>>>>> origin/develop
->>>>>>> b1cd7fc (.)
     }
 }
