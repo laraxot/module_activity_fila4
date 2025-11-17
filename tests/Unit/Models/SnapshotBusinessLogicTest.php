6b3558608cf75f577b9523d23b98a9cd296e6fa4
marco76tv <marco.sottana@gmail.com>
2025-11-17 15:51:06 +0100

    .

--- tests/Unit/Models/SnapshotBusinessLogicTest.php
+++ tests/Unit/Models/SnapshotBusinessLogicTest.php
@@ -21,7 +21,11 @@ describe('Snapshot Business Logic', function () {
             'aggregate_version',
             'state',
             'created_at',
+<<<<<<< HEAD
             'updated_at',
+=======
+            'updated_at'
+>>>>>>> 0a00ff2 (.)
         ];
 
         expect($snapshot->getFillable())->toEqual($expectedFillable);
