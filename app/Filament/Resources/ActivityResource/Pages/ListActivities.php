934f7ac38f38e080114d4081a9beafac3660cf93
marco76tv <marco.sottana@gmail.com>
2025-11-17 15:51:21 +0100

    .

--- app/Filament/Resources/ActivityResource/Pages/ListActivities.php
+++ app/Filament/Resources/ActivityResource/Pages/ListActivities.php
@@ -18,6 +18,14 @@ class ListActivities extends XotBaseListRecords
     public function getTableColumns(): array
     {
         return [
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> b1cd7fc (.)
             TextColumn::make('id')->sortable()->searchable(),
             TextColumn::make('description')->searchable()->limit(50),
             TextColumn::make('subject_type')->searchable(),
@@ -25,6 +33,36 @@ class ListActivities extends XotBaseListRecords
             TextColumn::make('causer_type')->searchable(),
             TextColumn::make('causer_id')->searchable(),
             TextColumn::make('created_at')->dateTime()->sortable(),
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+            TextColumn::make('id')
+                ->sortable()
+                ->searchable(),
+            TextColumn::make('description')
+                ->searchable()
+                ->limit(50),
+            TextColumn::make('subject_type')
+                ->searchable(),
+            TextColumn::make('subject_id')
+                ->searchable(),
+            TextColumn::make('causer_type')
+                ->searchable(),
+            TextColumn::make('causer_id')
+                ->searchable(),
+            TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable(),
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> b1cd7fc (.)
         ];
     }
 }
