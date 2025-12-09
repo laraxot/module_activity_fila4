1f37280892981fc05ba04e3cb2760aea6d1eeef9
marco76tv <marco.sottana@gmail.com>
2025-12-09 15:51:13 +0100

    .

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 /*
+<<<<<<< HEAD
  * |--------------------------------------------------------------------------
  * | API Routes
  * |--------------------------------------------------------------------------
@@ -12,6 +13,17 @@ declare(strict_types=1);
  * | is assigned the "api" middleware group. Enjoy building your API!
  * |
  */
+=======
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+>>>>>>> 0a00ff2 (.)
 
 // Route::middleware('auth:api')->get('/activity', function (Request $request) {//
 //    return $request->user();
