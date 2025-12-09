9a391054b4ff48e103005798ddd7f63896b8a9ad
marco76tv <marco.sottana@gmail.com>
2025-12-09 15:53:31 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 /*
+<<<<<<< HEAD
  * |--------------------------------------------------------------------------
  * | Web Routes
  * |--------------------------------------------------------------------------
@@ -17,3 +18,19 @@ declare(strict_types=1);
  * Route::get('/', 'ActivityController@index');
  * });
  */
+=======
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::prefix('activity')->group(function () {
+    Route::get('/', 'ActivityController@index');
+});
+*/
+>>>>>>> 0a00ff2 (.)
