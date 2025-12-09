5a59a56f2ddd064029a10224e9c7b4419fe97a3f
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:24:27 +0100

    .

--- resources/views/filament/tables/columns/event-properties.blade.php
+++ resources/views/filament/tables/columns/event-properties.blade.php
@@ -1,29 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 18dcd64 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 0a00ff2 (.)
-=======
->>>>>>> 18dcd64 (.)
 @foreach($getState() as $variable => $value)
     <p>
         {{$variable}}={{$value}}
         @if(!$loop->last),
         @endif
     </p>
-<<<<<<< HEAD
-<<<<<<< HEAD
 @endforeach
-=======
-@endforeach
->>>>>>> 0a00ff2 (.)
-=======
-@endforeach
->>>>>>> 18dcd64 (.)
