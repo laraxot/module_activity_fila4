65cd9c1f0fece496a61b734f0395ebdaca67bf5f
marco76tv <marco.sottana@gmail.com>
2025-12-09 15:53:09 +0100

    .

--- tests/Unit/Models/BaseModelTest.php
+++ tests/Unit/Models/BaseModelTest.php
@@ -4,21 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Activity\Tests\Unit\Models;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Modules\Activity\Models\BaseModel;
-=======
-use Modules\Activity\Models\BaseModel;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Database\Eloquent\Model;
->>>>>>> 0a00ff2 (.)
-=======
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Modules\Activity\Models\BaseModel;
->>>>>>> 18dcd64 (.)
 use Tests\TestCase;
 
 /**
