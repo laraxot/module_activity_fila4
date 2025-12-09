5d6678a0edbf0a1e4dc47ab589762bba1b59a07d
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:14:12 +0100

    .

--- tests/Unit/Models/BaseModelTest.php
+++ tests/Unit/Models/BaseModelTest.php
@@ -4,9 +4,33 @@ declare(strict_types=1);
 
 namespace Modules\Activity\Tests\Unit\Models;
 
+<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Modules\Activity\Models\BaseModel;
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\Activity\Models\BaseModel;
+=======
+use Modules\Activity\Models\BaseModel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Database\Eloquent\Model;
+>>>>>>> a12f125f4a (.)
+=======
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\Activity\Models\BaseModel;
+>>>>>>> b93ef594b4 (.)
+=======
+use Modules\Activity\Models\BaseModel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Database\Eloquent\Model;
+>>>>>>> origin/develop
+>>>>>>> b1cd7fc (.)
 use Tests\TestCase;
 
 uses(TestCase::class, RefreshDatabase::class);
