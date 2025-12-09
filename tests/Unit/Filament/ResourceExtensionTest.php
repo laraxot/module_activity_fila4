0aba936d9ba689551a4dedc2e7bce017ae048eda
marco76tv <marco.sottana@gmail.com>
2025-12-09 10:21:00 +0100

    .

--- tests/Unit/Filament/ResourceExtensionTest.php
+++ tests/Unit/Filament/ResourceExtensionTest.php
@@ -115,7 +115,11 @@ test('activity resource has correct model configuration', function () {
 });
 
 test('activity resource form schema returns array', function () {
+<<<<<<< HEAD
     $form = ActivityResource::getFormSchema();
+=======
+    $schema = ActivityResource::getFormSchema();
+>>>>>>> 8a1d805 (.)
 
     expect($form)->toBeArray()->not->toBeEmpty();
 
@@ -130,7 +134,11 @@ test('activity resource form schema returns array', function () {
 });
 
 test('snapshot resource form schema returns array', function () {
+<<<<<<< HEAD
     $form = SnapshotResource::getFormSchema();
+=======
+    $schema = SnapshotResource::getFormSchema();
+>>>>>>> 8a1d805 (.)
 
     expect($form)->toBeArray()->not->toBeEmpty();
 
@@ -143,7 +151,11 @@ test('snapshot resource form schema returns array', function () {
 });
 
 test('stored event resource form schema returns array', function () {
+<<<<<<< HEAD
     $form = StoredEventResource::getFormSchema();
+=======
+    $schema = StoredEventResource::getFormSchema();
+>>>>>>> 8a1d805 (.)
 
     expect($form)->toBeArray()->not->toBeEmpty();
 
