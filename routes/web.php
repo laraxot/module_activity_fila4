99f9436d8957589f2dab1e4fd996cb1bfc07f0f9
marco76tv <marco.sottana@gmail.com>
2025-12-09 15:43:29 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,6 @@
 declare(strict_types=1);
 
 /*
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> b1cd7fc (.)
  * |--------------------------------------------------------------------------
  * | Web Routes
  * |--------------------------------------------------------------------------
@@ -20,43 +12,8 @@ declare(strict_types=1);
  * | contains the "web" middleware group. Now create something great!
  * |
  */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b1cd7fc (.)
 
 /*Route::prefix('activity')->group(function () {
  * Route::get('/', 'ActivityController@index');
  * });
  */
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> origin/develop
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-/*Route::prefix('activity')->group(function () {
-    Route::get('/', 'ActivityController@index');
-});
-*/
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-
-/*Route::prefix('activity')->group(function () {
- * Route::get('/', 'ActivityController@index');
- * });
- */
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> b1cd7fc (.)
