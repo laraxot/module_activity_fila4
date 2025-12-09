9e4394ef19f01e34da54a1b3010cd3d60fc6894e
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:07:10 +0100

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> b1cd7fc (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> b1cd7fc (.)
 @extends('activity::layouts.master')
 
 @section('content')
