d49857b4a4b98c8536c747fe7317ffb6bc1dcefb
marco76tv <marco.sottana@gmail.com>
2025-12-16 18:45:21 +0100

    .

--- lang/it/edit_activity.php
+++ lang/it/edit_activity.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 18dcd64 (.)
 declare(strict_types=1);
 
 return [
@@ -52,25 +48,3 @@ return [
         'properties.json' => 'Le proprietà devono essere un JSON valido',
     ],
 ];
-<<<<<<< HEAD
-=======
-return array (
-  'actions' => 
-  array (
-    'delete' => 
-    array (
-      'label' => 'delete',
-    ),
-    'save' => 
-    array (
-      'label' => 'save',
-    ),
-    'cancel' => 
-    array (
-      'label' => 'cancel',
-    ),
-  ),
-);
->>>>>>> 0a00ff2 (.)
-=======
->>>>>>> 18dcd64 (.)
