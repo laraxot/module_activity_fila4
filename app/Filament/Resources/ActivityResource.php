c06a5970ddd6566b5fc762cdaf652cae3498b2e2
marco76tv <marco.sottana@gmail.com>
2025-12-16 13:09:59 +0100

    .

--- app/Filament/Resources/ActivityResource.php
+++ app/Filament/Resources/ActivityResource.php
@@ -31,20 +31,20 @@ class ActivityResource extends XotBaseResource
     /**
      * Define the form schema for the Activity resource.
      *
-     * @return array<int, Component>
+     * @return array<string, Component>
      */
     #[Override]
     public static function getFormSchema(): array
     {
         return [
-            TextInput::make('log_name')->required()->maxLength(255),
-            TextInput::make('description')->required()->maxLength(255),
-            TextInput::make('subject_type')->required()->maxLength(255),
-            TextInput::make('subject_id')->numeric()->required(),
-            TextInput::make('causer_type')->maxLength(255),
-            TextInput::make('causer_id')->numeric(),
-            KeyValue::make('properties')->columnSpanFull(),
-            TextInput::make('batch_uuid')->maxLength(36),
+            'log_name' => TextInput::make('log_name')->required()->maxLength(255),
+            'description' => TextInput::make('description')->required()->maxLength(255),
+            'subject_type' => TextInput::make('subject_type')->required()->maxLength(255),
+            'subject_id' => TextInput::make('subject_id')->numeric()->required(),
+            'causer_type' => TextInput::make('causer_type')->maxLength(255),
+            'causer_id' => TextInput::make('causer_id')->numeric(),
+            'properties' => KeyValue::make('properties')->columnSpanFull(),
+            'batch_uuid' => TextInput::make('batch_uuid')->maxLength(36),
         ];
     }
 }
