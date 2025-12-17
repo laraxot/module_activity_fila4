8f6fca544e93872fad2526a25a72a23402d5e5bc
marco76tv <marco.sottana@gmail.com>
2025-12-17 09:20:00 +0100

    .

--- tests/Unit/Models/BaseModelTest.php
+++ tests/Unit/Models/BaseModelTest.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Modules\Activity\Models\BaseModel;
 use Modules\Activity\Tests\TestCase;
 
+<<<<<<< HEAD
 uses(TestCase::class);
 
 /**
@@ -50,4 +51,51 @@ test('base model has timestamps enabled', function (): void {
     $model = makeTestActivityModel();
 
     expect($model->usesTimestamps())->toBeTrue();
+=======
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+class TestActivityBaseModel extends BaseModel
+{
+    /** @var string */
+    protected $table = 'test_activity_table';
+}
+
+uses(TestCase::class, RefreshDatabase::class);
+
+<<<<<<< HEAD
+test('base model extends eloquent model', function (): void {
+    $baseModel = new TestActivityBaseModel;
+    expect($baseModel)->toBeInstanceOf(Model::class);
+=======
+beforeEach(function () {
+    $this->baseModel = new class extends BaseModel
+    {
+        protected $table = 'test_activity_table';
+    };
+>>>>>>> 9baa519 (.)
+});
+
+test('base model has correct table name', function (): void {
+    $baseModel = new TestActivityBaseModel;
+    expect($baseModel->getTable())->toBe('test_activity_table');
+});
+
+test('base model can be instantiated', function (): void {
+    $baseModel = new TestActivityBaseModel;
+    expect($baseModel)->toBeInstanceOf(BaseModel::class);
+});
+
+test('base model has proper inheritance chain', function (): void {
+    $baseModel = new TestActivityBaseModel;
+    expect($baseModel)->toBeInstanceOf(BaseModel::class);
+    expect($baseModel)->toBeInstanceOf(Model::class);
+});
+
+test('base model has timestamps enabled', function (): void {
+    $baseModel = new TestActivityBaseModel;
+    expect($baseModel->timestamps)->toBeTrue();
+>>>>>>> 1e9f71a (.)
 });
