f8102d29b74522b35575ba096f4ffd1abd382806
marco76tv <marco.sottana@gmail.com>
2025-12-18 11:04:22 +0100

    .

--- app/Traits/HasEvents.php
+++ app/Traits/HasEvents.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace Modules\Activity\Traits;
 
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Modules\Activity\Models\StoredEvent;
 use Modules\Activity\Models\Snapshot;
 
 trait HasEvents
 {
-    public function storedEvents()
+    public function storedEvents(): MorphMany
     {
         return $this->morphMany(StoredEvent::class, 'aggregate');
     }
 
-    public function snapshots()
+    public function snapshots(): MorphMany
     {
         return $this->morphMany(Snapshot::class, 'aggregate');
     }
