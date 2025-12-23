168a0545fe15ee8cd80f9d1cbb1a8e49bc3425bb
marco76tv <marco.sottana@gmail.com>
2025-12-23 08:55:39 +0100

    .

--- resources/lang/en/activity-resource.php
+++ resources/lang/en/activity-resource.php
@@ -1,25 +1,12 @@
 <?php
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 declare(strict_types=1);
 
 
-=======
->>>>>>> 0a00ff2 (.)
-=======
-declare(strict_types=1);
-
-
->>>>>>> 18dcd64 (.)
 return [
     'fields' => [
         'id' => [
             'label' => 'ID',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 18dcd64 (.)
             'tooltip' => 'Unique identifier of the activity',
         ],
         'description' => [
@@ -46,74 +33,21 @@ return [
             'label' => 'Created At',
             'tooltip' => 'Date and time when the activity was created',
         ],
-<<<<<<< HEAD
-=======
-            'tooltip' => 'Unique identifier of the activity'
-        ],
-        'description' => [
-            'label' => 'Description',
-            'tooltip' => 'Description of the activity'
-        ],
-        'subject_type' => [
-            'label' => 'Subject Type',
-            'tooltip' => 'Type of entity subject to the activity'
-        ],
-        'subject_id' => [
-            'label' => 'Subject ID',
-            'tooltip' => 'Identifier of the entity subject to the activity'
-        ],
-        'causer_type' => [
-            'label' => 'Causer Type',
-            'tooltip' => 'Type of entity that caused the activity'
-        ],
-        'causer_id' => [
-            'label' => 'Causer ID',
-            'tooltip' => 'Identifier of the entity that caused the activity'
-        ],
-        'created_at' => [
-            'label' => 'Created At',
-            'tooltip' => 'Date and time when the activity was created'
-        ]
->>>>>>> 0a00ff2 (.)
-=======
->>>>>>> 18dcd64 (.)
     ],
     'actions' => [
         'view' => [
             'label' => 'View',
-<<<<<<< HEAD
-<<<<<<< HEAD
             'tooltip' => 'View activity details',
-=======
-            'tooltip' => 'View activity details'
->>>>>>> 0a00ff2 (.)
-=======
-            'tooltip' => 'View activity details',
->>>>>>> 18dcd64 (.)
         ],
         'delete' => [
             'label' => 'Delete',
             'tooltip' => 'Delete this activity',
-<<<<<<< HEAD
-<<<<<<< HEAD
             'confirmation' => 'Are you sure you want to delete this activity?',
         ],
-=======
-            'confirmation' => 'Are you sure you want to delete this activity?'
-        ]
->>>>>>> 0a00ff2 (.)
-=======
-            'confirmation' => 'Are you sure you want to delete this activity?',
-        ],
->>>>>>> 18dcd64 (.)
     ],
     'filters' => [
         'date' => [
             'label' => 'Date',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 18dcd64 (.)
             'tooltip' => 'Filter by creation date',
         ],
         'type' => [
@@ -122,16 +56,3 @@ return [
         ],
     ],
 ];
-<<<<<<< HEAD
-=======
-            'tooltip' => 'Filter by creation date'
-        ],
-        'type' => [
-            'label' => 'Type',
-            'tooltip' => 'Filter by activity type'
-        ]
-    ]
-]; 
->>>>>>> 0a00ff2 (.)
-=======
->>>>>>> 18dcd64 (.)
